9476f3e105c83e410ab2cd2d4345970e180152ff
h-waji <goo.hiro.w@gmail.com>
2026-09-05 15:27:09 +0900

    投稿一覧APIでJSONデータを返却

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.Aeson
+import qualified Data.Text.Lazy as TL
 import Web.Scotty
 
 main :: IO ()
-main =
+main = do
+  postsResult <- eitherDecodeFileStrict "data/posts.json" :: IO (Either String [Value])
+
   scotty 3000 $ do
     get "/health" $ do
       text "OK"
+
+    get "/api/v1/posts" $ do
+      case postsResult of
+        Right posts ->
+          json $
+            object
+              [ "meta"
+                  .= object
+                    [ "api"
+                        .= object
+                          [ "name" .= ("haskell-api" :: String),
+                            "language" .= ("Haskell" :: String),
+                            "category" .= ("public" :: String)
+                          ],
+                      "count" .= length posts
+                    ],
+                "data"
+                  .= object
+                    [ "posts" .= posts
+                    ]
+              ]
+
+        Left err ->
+          text ("Failed to load posts.json: " <> TL.pack err)
