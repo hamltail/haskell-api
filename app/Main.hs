12a66b5a87fbdc0dac14a8a4cfb484a77d7021c1
h-waji <goo.hiro.w@gmail.com>
2026-09-05 16:43:01 +0900

    PORTを環境変数から取得

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,8 @@
 
 import App (routes)
 import Data.Aeson
-import System.Environment (getEnv)
+import System.Environment (getEnv, lookupEnv)
+import Text.Read (readMaybe)
 import Web.Scotty
 
 main :: IO ()
@@ -11,7 +12,16 @@ main = do
   apiLanguage <- getEnv "API_LANGUAGE"
   apiCategory <- getEnv "API_CATEGORY"
   apiKey <- getEnv "API_KEY"
+  port <- getPort
   postsResult <- eitherDecodeFileStrict "data/posts.json" :: IO (Either String [Value])
 
-  scotty 3000 $
+  scotty port $
     routes apiName apiLanguage apiCategory apiKey postsResult
+
+getPort :: IO Int
+getPort = do
+  portValue <- lookupEnv "PORT"
+  pure $
+    case portValue >>= readMaybe of
+      Just port -> port
+      Nothing -> 3000
