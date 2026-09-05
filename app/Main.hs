4d32dceed5827b9fec8076c09bd017d2e149dd1f
h-waji <goo.hiro.w@gmail.com>
2026-09-05 15:46:36 +0900

    投稿詳細APIを実装

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.Aeson
+import qualified Data.Aeson.KeyMap as KM
+import Data.List (find)
+import Data.Scientific (toBoundedInteger)
 import qualified Data.Text.Lazy as TL
+import Network.HTTP.Types.Status (internalServerError500, notFound404)
 import Web.Scotty
+
+findPost :: Int -> [Value] -> Maybe Value
+findPost postId = find matchesPost
+  where
+    matchesPost (Object postObject) =
+      case KM.lookup "id" postObject of
+        Just (Number value) -> toBoundedInteger value == Just postId
+        _ -> False
+    matchesPost _ = False
 
 main :: IO ()
 main = do
@@ -32,6 +45,27 @@ main = do
                     [ "posts" .= posts
                     ]
               ]
+        Left err -> do
+          status internalServerError500
+          text ("Failed to load posts.json: " <> TL.pack err)
 
-        Left err ->
+    get "/api/v1/posts/:id" $ do
+      postId <- pathParam "id"
+      case postsResult of
+        Right posts ->
+          case findPost postId posts of
+            Just postValue ->
+              json postValue
+            Nothing -> do
+              status notFound404
+              json $
+                object
+                  [ "error"
+                      .= object
+                        [ "code" .= ("NOT_FOUND" :: String),
+                          "message" .= ("Post not found" :: String)
+                        ]
+                  ]
+        Left err -> do
+          status internalServerError500
           text ("Failed to load posts.json: " <> TL.pack err)
